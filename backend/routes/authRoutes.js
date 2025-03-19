4a6898269edfde2e03a1fdf426cfe1c4cf7899d1
Bilalik369 <bilal11iken@gmail.com>
2025-03-19 10:34:31 +0000

    Ajout de la fonction register pour l'inscription des utilisateurs

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,27 @@
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password, role } = req.body
+
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+      return res.status(400).json({ message: "Cet utilisateur existe déjà" })
+    }
+
+    
+    const newUser = new User({
+      username,
+      password,
+      role: role || "user",
+    })
+
+    await newUser.save()
+
+    res.status(201).json({ message: "Utilisateur créé avec succès" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
